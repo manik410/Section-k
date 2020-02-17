ff5d254c6028556775a7dad0c52f5cd3e2b8aed0
Manik Jindal <manikjindal140@gmail.com>
2020-02-17 22:18:55 +0530

    Add files via upload

---
+++ inversePermutation.java
@@ -0,0 +1,38 @@
+package practice;
+import java.util.*;
+public class inversePermutation 
+{
+	static void create(int[] a)
+	{
+		for(int i=0;i<a.length;i++)
+		{
+			for(int j=0;j<a.length;j++)
+			{
+				if(a[j]==i+1)
+				{
+					System.out.print(j+1+" ");
+					break;
+				}
+			}
+		}
+		System.out.println();
+	}
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the no of testcases");
+		int test=sc.nextInt();
+		for(int i=0;i<test;i++)
+		{
+			System.out.println("Enter the size");
+			int size=sc.nextInt();
+			int[]a=new int[size];
+			for(int j=0;j<size;j++)
+			{
+				a[j]=sc.nextInt();
+			}
+			create(a);
+		}
+	}
+
+}
